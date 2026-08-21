c1de80b00dcb67bde9067320b1199fc5591498b4
Nai64 <k3gn@proton.me>
2026-08-22 00:56:25 +0500

    feat: add Bypass OkHttp Pinning patch

--- patches/src/main/kotlin/util/PatchListGenerator.kt
+++ patches/src/main/kotlin/util/PatchListGenerator.kt
@@ -80,6 +80,7 @@ private fun generatePatchList(version: String, patches: Set<Patch<*>>) {
         "Unlock Max Aspect Ratio",
         "Bypass VPN Detection",
         "Spoof Device Model",
+        "Bypass OkHttp Pinning",
         "Spoof Play Integrity",
         "Bypass License Verification",
         "Bypass Google Play Services Check",
