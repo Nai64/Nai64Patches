5c2b9ed439a54e43e97a846ef84222508b698ce4
Nai64 <k3gn@proton.me>
2026-08-22 01:22:31 +0500

    feat: add Treat Network as Unmetered patch

--- patches/src/main/kotlin/util/PatchListGenerator.kt
+++ patches/src/main/kotlin/util/PatchListGenerator.kt
@@ -91,6 +91,7 @@ private fun generatePatchList(version: String, patches: Set<Patch<*>>) {
         "Enable WebView Debugging",
         "Fake Battery Whitelist",
         "Spoof Signature Match",
+        "Treat Network as Unmetered",
         "Grant All-Files Access",
         "Hide Roaming Status",
         "Ignore Power Save Mode",
