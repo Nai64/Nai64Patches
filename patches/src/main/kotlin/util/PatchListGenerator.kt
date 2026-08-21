89a161a4ef5a13f3cd9925ec19cecf08b06bfb4a
Nai64 <k3gn@proton.me>
2026-08-22 01:22:28 +0500

    feat: add Ignore GPS Disabled patch

--- patches/src/main/kotlin/util/PatchListGenerator.kt
+++ patches/src/main/kotlin/util/PatchListGenerator.kt
@@ -91,6 +91,7 @@ private fun generatePatchList(version: String, patches: Set<Patch<*>>) {
         "Enable WebView Debugging",
         "Fake Battery Whitelist",
         "Spoof Signature Match",
+        "Ignore GPS Disabled",
         "Hide Mock Location",
         "Spoof Play Integrity",
         "Bypass License Verification",
