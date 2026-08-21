97ad5a09998f308947a214f2d6269fa328bcb210
Nai64 <k3gn@proton.me>
2026-08-22 01:22:29 +0500

    feat: add Force Notifications Enabled patch

--- patches/src/main/kotlin/util/PatchListGenerator.kt
+++ patches/src/main/kotlin/util/PatchListGenerator.kt
@@ -91,6 +91,7 @@ private fun generatePatchList(version: String, patches: Set<Patch<*>>) {
         "Enable WebView Debugging",
         "Fake Battery Whitelist",
         "Spoof Signature Match",
+        "Force Notifications Enabled",
         "Ignore Screen Lock",
         "Ignore GPS Disabled",
         "Hide Mock Location",
