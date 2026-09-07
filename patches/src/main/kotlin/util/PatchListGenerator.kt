4e45b46f26aa79c456b4d44d4ad880b56eb50229
Nai64 <k3gn@proton.me>
2026-09-07 23:01:57 +0500

    feat: drop experimental suffix from featured patches

--- patches/src/main/kotlin/util/PatchListGenerator.kt
+++ patches/src/main/kotlin/util/PatchListGenerator.kt
@@ -38,11 +38,11 @@ private fun generatePatchList(version: String, patches: Set<Patch<*>>) {
 
     val patchOrder = listOf(
         "★ Unlock Premium",
-        "★ Free In-app Purchases (Experimental)",
-        "★ No Ads (Experimental)",
-        "★ Ads Free Rewards (Experimental)",
+        "★ Free In-app Purchases",
+        "★ No Ads",
+        "★ Ads Free Rewards",
         "★ Unlimited Currencies",
-        "★ Pairip Bypass (Experimental)",
+        "★ Pairip Bypass",
         "No Ads",
         "Skip Consent Popup",
         "Skip Rate-Us Prompt",
